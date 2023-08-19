b524cd318890f5e4aa8fdb2606cc75c675d6fae4
zachattack531 <26945971+zachattack531@users.noreply.github.com>
2023-08-19 18:51:55 -0400

    Added in Brake Multiplier from last years' code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DriveCmd.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DriveCmd.java
@@ -25,22 +25,33 @@ public class DriveCmd extends CommandBase {
 
     @Override
     public void execute(){
-        double multiplier = 1-gamepad.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER);
+        double brakeMultiplier = 1;
+        double rightTrigger=gamepad.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER);
+        // if precision mode is on (the right trigger is pulled down to some degree)
+        if (rightTrigger > 0.05 && rightTrigger < 0.75) {
+            brakeMultiplier = 1 - rightTrigger;
+            //telemetry.addData("Precise Mode", "On");
+            // also if precision mode is on, but it caps brake at 25% after 75% trigger
+        } else if (rightTrigger >= 0.75) {
+            brakeMultiplier = 0.25;
+            //telemetry.addData("Precise Mode", "On");
+        }
+
         if (fieldCentric) {
             // optional fifth parameter for squared inputs
             drivetrainSub.getDrive().driveFieldCentric(
-                    gamepad.getLeftX()*multiplier,
-                    gamepad.getLeftY()*multiplier,
-                    gamepad.getRightX()*multiplier,
+                    gamepad.getLeftX()*brakeMultiplier,
+                    gamepad.getLeftY()*brakeMultiplier,
+                    gamepad.getRightX()*brakeMultiplier,
                     angleDegrees.getAsDouble(),   // gyro value passed in here must be in degrees
                     false
             ); ;
         } else {
             // optional fourth parameter for squared inputs
             drivetrainSub.getDrive().driveRobotCentric(
-                    gamepad.getLeftX()*multiplier,
-                    gamepad.getLeftY()*multiplier,
-                    gamepad.getRightX()*multiplier,
+                    gamepad.getLeftX()*brakeMultiplier,
+                    gamepad.getLeftY()*brakeMultiplier,
+                    gamepad.getRightX()*brakeMultiplier,
                     false
             );
         }
